2206cb750747f55b595fb114ef9cb6d846ece7c2
Sergio Martínez Portela <sergio@codigoparallevar.com>
2025-03-24 10:42:32 +0100

    Add script to pull datasets.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # fine-tuning
+
+This repository holds the code to fine-tune LLMs on mapping natural language queries into SPARQL queries.
+
+## Preparation
+
+### Pull data
+
+Run `python3 scripts/pull_datasets.py` (no dependencies).
+
+## References
+
+### Datasets used
+
+The following datasets are used for this fine tuning. Most of them provide separate sets for training and testing.
+
+- **qald-9**: https://github.com/ag-sc/QALD/
+- **qald-10**: https://github.com/KGQA/QALD-10/
+- **bestiary**: https://github.com/danrd/sparqlgen/
+- **VQuAnDA**: https://github.com/AskNowQA/VQUANDA/
+- **LC-QuAD 1.0**: https://github.com/AskNowQA/LC-QuAD/
+- **LC-QuAD 2.0**: https://github.com/AskNowQA/LC-QuAD2.0/
+- **DBNQA**: https://github.com/AKSW/DBNQA
---
+++ scripts/pull_datasets.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import urllib.request
+import tempfile
+import zipfile
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATASET_DIR = os.path.join(BASE_DIR, "datasets")
+DATASET_JSON = os.path.join(DATASET_DIR, "datasets.json")
+
+
+def dataset_name_to_slug(ds_name: str) -> str:
+    """Convert a dataset name to a format more adequate for directory name."""
+    assert ":" not in ds_name
+    return ds_name.lower().replace(" ", "_")
+
+
+def pull_datasets():
+    """Pull all known datasets to the `datasets` directory."""
+    with open(DATASET_JSON, "rt") as f:
+        dataset_data = json.load(f)
+
+    for dataset_name, dataset_urls in dataset_data.items():
+        if dataset_name.startswith("__"):
+            # Skip
+            continue
+
+        dataset_path = os.path.join(DATASET_DIR, dataset_name_to_slug(dataset_name))
+
+        pull_dataset(dataset_path, dataset_name, dataset_urls)
+
+
+def pull_dataset(path: str, name: str, urls: dict[str, str]):
+    """Download the files contained in a dataset."""
+    os.makedirs(path, exist_ok=True)
+    if "all" in urls:
+        # All dataset in a single file
+        if os.path.exists(
+            os.path.join(
+                path,
+                "all.json",
+            )
+        ):
+            print(f"✔ {name} unified dataset present")
+        else:
+            print(f"  ↓  Downloading {name} dataset")
+            if urls["all"].endswith(".json"):
+                urllib.request.urlretrieve(urls["all"], os.path.join(path, "all.json"))
+            else:
+                # Zip file
+                zip_file = download_temp_zip_file(urls["all"])
+                # Expect a single file inside
+                inner_files = zip_file.namelist()
+                assert (
+                    len(inner_files) == 1
+                ), "Expected a file inside the zip, found: {}".format(inner_files)
+                with zip_file.open(inner_files[0], "rt") as finner:
+                    with open(os.path.join(path, "all.json"), "wt") as fouter:
+                        fouter.write(finner.read())
+
+            print(f"  ✔  {name} dataset ready")
+
+    else:
+        # Dataset pre-splitted in train and test
+        assert (
+            "train" in urls and "test" in urls
+        ), "Expected 'train' and 'test' in URLs, found: {}".format(urls.keys())
+
+        print("{} | Split dataset...".format(name))
+        for key in "train", "test":
+            assert urls[key].endswith(".json")
+            if os.path.exists(os.path.join(path, key + ".json")):
+                print(f"  ✔  {key.title()} file present")
+            else:
+                print(f"  ↓  Downloading {key} file")
+                urllib.request.urlretrieve(urls[key], os.path.join(path, key + ".json"))
+                print(f"  ✔  {key.title()} file ready")
+
+
+def download_temp_zip_file(url: str) -> zipfile.ZipFile:
+    """Download a Zip file to a temporary location and open it."""
+    with tempfile.NamedTemporaryFile(suffix=".zip") as f:
+        urllib.request.urlretrieve(
+            url,
+            f.name,
+        )
+        return zipfile.ZipFile(f.name)
+
+
+if __name__ == "__main__":
+    pull_datasets()
