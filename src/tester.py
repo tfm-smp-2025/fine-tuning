a0fb681bd27eb19564a8f10b2cf1145aa5a22d6e
Sergio Martínez Portela <sergio@codigoparallevar.com>
2025-04-02 00:08:24 +0200

    Handle errors on tester calls.

--- src/tester.py
+++ src/tester.py
@@ -15,5 +15,8 @@ def run_test(args):
     TEST_PROMPT = "when was father chris riley born?"
     for translator in all_translators:
         logging.info("({}) INPUT: {}".format(translator, TEST_PROMPT))
-        result = translator.translate(TEST_PROMPT)
-        logging.info("({}) RESULT: {}".format(translator, result))
+        try:
+            result = translator.translate(TEST_PROMPT)
+            logging.info("({}) RESULT: {}".format(translator, result))
+        except:
+            logging.exception("({}) EXCEPTION".format(translator))
