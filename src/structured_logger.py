919ac9110da357c19b776dd77ef4f41e286e44f4
Sergio Martínez Portela <sergio@codigoparallevar.com>
2025-04-15 10:17:16 +0200

    Add structured, persistent, logger.

---
+++ src/structured_logger.py
@@ -0,0 +1,113 @@
+import os
+import datetime
+import logging
+import time
+import json
+import traceback
+
+from typing import Optional
+
+class StructuredLoggerContext:
+    def __init__(self, parent: 'StructuredLogger', context_name=None):
+        self.name = context_name
+        self.parent = parent
+        self.root_logger = logging.getLogger()
+        self.previous_handler = None
+        self.level = 0
+
+    def __enter__(self):
+        print("Inserting handler")
+        # Add itself as handler
+        assert len(self.root_logger.handlers) in (0, 1)
+        if len(self.root_logger.handlers) == 1:
+            self.previous_handler = self.root_logger.handlers[0]
+            self.root_logger.removeHandler(self.previous_handler)
+
+        self.root_logger.addHandler(self)
+        return self
+
+    def __exit__(self, exc_type, exc_val, tb):
+        print("Removing handler")
+        # Restore handler
+        self.root_logger.removeHandler(self)
+        if self.previous_handler is not None:
+            self.root_logger.addHandler(self.previous_handler)
+
+    def handle(self, record: logging.LogRecord):
+        exception = None
+        if record.exc_info is not None:
+            exception = '\n'.join(
+                traceback.format_exception(
+                    record.exc_info[0], record.exc_info[1], record.exc_info[2]
+                )
+            )
+
+        self._log(
+            record.levelname,
+            record.getMessage(),
+            context=self,
+            exception=exception,
+        )
+
+    def _log(self, level, message, context, exception=None):
+        self.parent._log(level, message, context, exception)
+
+
+class StructuredLogger:
+    def __init__(self):
+        self.output_path = os.path.abspath("log-" + str(datetime.datetime.now()) + ".jsonl")
+        self.output = None
+
+    def context(self, context_name=None):
+        return StructuredLoggerContext(parent=self, context_name=context_name)
+
+    def debug(self, message, context=None):
+        self._log('debug', message, context)
+    def info(self, message, context=None):
+        self._log('info', message, context)
+    def warn(self, message, context=None):
+        self._log('warn', message, context)
+    def error(self, message, context=None):
+        self._log('error', message, context)
+    def fatal(self, message, context=None):
+        self._log('fatal', message, context)
+
+    def _log(self, level, message, context, exception=None):
+        self._print_log(level, message, context, exception)
+
+        data = {
+            "level": level,
+            "message": message,
+            "timestamp": time.time(),
+            "time": datetime.datetime.now().isoformat(),
+        }
+
+        if exception is not None:
+            data['exception'] = str(exception)
+
+        if context is not None:
+            data['context_name'] = context.name
+
+        with open(self.output_path, 'at') as f:
+            f.write(json.dumps(data) + '\n')
+
+    def _print_log(self, level, message, context, exception=None):
+        stime = datetime.datetime.now().isoformat()
+        ctxt = ''
+        if context:
+            ctxt = f'[{context.name}]\t'
+        print(f"{stime} {ctxt}{level}:\t \x1b[1m{message}\x1b[0m")
+        if exception:
+            print("---------- 8< ----------")
+            print(exception)
+            print("---------- >8 ----------")
+
+
+LOGGER: Optional[StructuredLogger] = None
+
+def get_logger() -> StructuredLogger:
+    global LOGGER
+    if LOGGER is None:
+        LOGGER = StructuredLogger()
+
+    return LOGGER
--- src/tester.py
+++ src/tester.py
@@ -6,11 +6,14 @@ from .translators import all_translators
 from .datasets import dataset_loader
 from .translators.utils import extract_code_blocks, CodeBlock
 from .ontology import Ontology, property_graph_to_rdf
+from .structured_logger import StructuredLogger, get_logger
 
 def run_test(args):
     """Run some tests over the configured models."""
     sample_size = args.sample
     is_full = args.full
+
+    logger = get_logger()
 
     assert (
         sample_size is not None
@@ -24,52 +27,50 @@ def run_test(args):
             ontology = Ontology(args.sparql_server, ds.sparql_endpoint)
 
         for translator in all_translators:
-            if translator.model.model_name not in args.models:
-                logging.debug("({}) SKIPPING, model not selected".format(translator))
-                continue
-
-            if ontology:
-                translator.set_ontology(ontology)
-
-            logging.info("({}) DATASET: {}".format(translator, ds))
-
-            dataset_counter = 0
-            for question in tqdm.tqdm(ds.get_test_data()):
-                if question.lang not in ('en', None):
-                    logging.debug('SKIPPING question in non-english: {}'.format(question.lang))
+            with logger.context("({}) DATASET: {}".format(translator, ds)) as ctxt:
+                if translator.model.model_name not in args.models:
+                    logging.debug("({}) SKIPPING, model not selected".format(translator))
                     continue
 
-                dataset_counter += 1
-                if sample_size is not None and dataset_counter > sample_size:
-                    logging.debug('Closing dataset after {} elements tested'.format(sample_size))
+                if ontology:
+                    translator.set_ontology(ontology)
 
-                logging.info("({}) INPUT: {}".format(translator, question.question))
+                dataset_counter = 0
+                for question in tqdm.tqdm(ds.get_test_data()):
+                    if question.lang not in ('en', None):
+                        logging.debug('SKIPPING question in non-english: {}'.format(question.lang))
+                        continue
 
-                try:
-                    if ontology:
-                        expected_result = ontology.run_query(question.answer)
-                        logging.info("({}) EXPECTED RESULT: {}".format(translator, expected_result))
+                    dataset_counter += 1
+                    if sample_size is not None and dataset_counter > sample_size:
+                        logging.debug('Closing dataset after {} elements tested'.format(sample_size))
 
-                    result = translator.translate(question.question)
+                    logging.info("({}) INPUT: {}".format(translator, question.question))
 
-                    logging.info("({}) RESULT query: {}".format(translator, result))
-                    if isinstance(result, CodeBlock):
-                        sparql_code_blocks = [result]
-                    else:
-                        sparql_code_blocks = [
-                            block.content
-                            for block in extract_code_blocks(text=result)
-                            if block.language.lower() == 'sparql'
-                        ]
+                    try:
+                        if ontology:
+                            expected_result = ontology.run_query(question.answer)
+                            logging.info("({}) EXPECTED RESULT: {}".format(translator, expected_result))
 
-                    if ontology:
-                        logging.info("({}) TESTING query: {}".format(translator, sparql_code_blocks[-1]))
-                        translator_result = ontology.run_query(sparql_code_blocks[-1].content)
-                        logging.info("({}) TRANSLATOR RESULT: {}".format(translator, translator_result))
+                        result = translator.translate(question.question)
 
-                except KeyboardInterrupt:
-                    logging.error("Stopping due to Keyboard Interrupt")
-                    raise
-                except:
-                    logging.exception("({}) EXCEPTION".format(translator))
-                    # raise
+                        logging.info("({}) RESULT query: {}".format(translator, result))
+                        if isinstance(result, CodeBlock):
+                            sparql_code_blocks = [result]
+                        else:
+                            sparql_code_blocks = [
+                                block.content
+                                for block in extract_code_blocks(text=result)
+                                if block.language.lower() == 'sparql'
+                            ]
+
+                        if ontology:
+                            logging.info("({}) TESTING query: {}".format(translator, sparql_code_blocks[-1]))
+                            translator_result = ontology.run_query(sparql_code_blocks[-1].content)
+                            logging.info("({}) TRANSLATOR RESULT: {}".format(translator, translator_result))
+
+                    except KeyboardInterrupt:
+                        logging.fatal("Stopping due to Keyboard Interrupt")
+                        raise
+                    except:
+                        logging.exception("({}) EXCEPTION".format(translator))
